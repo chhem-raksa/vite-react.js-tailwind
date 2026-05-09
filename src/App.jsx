7f5b9d2626e279284aea72c0ad37d9f5e81464cb
Chhem Raksa <121108024+chhemraksa@users.noreply.github.com>
2026-05-09 16:17:13 +0700

    First Commit of React development

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return(
+    <>
+    <section>
+      <center>
+        <h2 className="bg-green-400">Welcome to My App</h2>
+      </center>
+    </section>
+    </>
+  )
+}
+export default App;
